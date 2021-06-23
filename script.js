a9b17f45a01c9cd8c2cce811ceb180313e98ec38
Regis JP Garnier <regis.garnier@wanadoo.fr>
2021-06-23 16:03:49 +0000

    test after coding all but with env bug

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    // FORM DATA IMPORT //
+    var validatedPrincipal = validatePrincipal(); // or launch onblur by HTML
+    if (validatedPrincipal) {
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        // CALCULATED DATA //
+        var interest = Number(principal) * Number(years) * Number(rate) /100.00;
+        var year = new Date().getFullYear()+parseInt(years); // WARNING : on 'year' neq 'years"
+        var amount = Number(principal) + Number(interest)
+        // OUTPUT //
+        document.getElementById("result").innerHTML="If you deposit <b>"+principal+"</b>,\<br\>at an interest rate of <b>"+rate+"%</b>\<br\>You will receive an amount of <b>"+amount+"</b>,\<br\>in the year <b>"+year+"</b>\<br\>"
+    }
+}       
+   
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validatePrincipal(){
+       var principal = document.getElementById("principal").value;
+       if ( principal == '' || principal == undefined || principal <1){ //test empty, not number, leq 0 or 0
+           alert("Enter a positive number").blur();
+           document.getElementById(myElementID).focus("principal");
+           return false;
+        }
+        return true;
 }
         
